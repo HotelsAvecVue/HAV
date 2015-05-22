9909ca9535e4ba0b6a043ef40fa787398e9c71c2
HotelsAvecVue <amichallet@hotelsavecvue.com>
2015-05-22 17:17:26 +0200

    Panneau d'admin - Résolution du bug d'affichage
    Problème venant des routes (surtout pour les catégories)

--- src/Ens/JobeetBundle/Controller/CategoryController.php
+++ src/Ens/JobeetBundle/Controller/CategoryController.php
@@ -14,7 +14,79 @@ use Ens\JobeetBundle\Form\CategoryType;
  */
 class CategoryController extends Controller
 {
-    
+
+    /**
+     * Lists all Category entities.
+     *
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('JobeetBundle:Category')->findAll();
+
+        return $this->render('JobeetBundle:Category:index.html.twig', array(
+            'entities' => $entities,
+        ));
+    }
+    /**
+     * Creates a new Category entity.
+     *
+     */
+    public function createAction(Request $request)
+    {
+        $entity = new Category();
+        $form = $this->createCreateForm($entity);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('category_show', array('id' => $entity->getId())));
+        }
+
+        return $this->render('JobeetBundle:Category:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    /**
+     * Creates a form to create a Category entity.
+     *
+     * @param Category $entity The entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createCreateForm(Category $entity)
+    {
+        $form = $this->createForm(new CategoryType(), $entity, array(
+            'action' => $this->generateUrl('category_create'),
+            'method' => 'POST',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Create'));
+
+        return $form;
+    }
+
+    /**
+     * Displays a form to create a new Category entity.
+     *
+     */
+    public function newAction()
+    {
+        $entity = new Category();
+        $form   = $this->createCreateForm($entity);
+
+        return $this->render('JobeetBundle:Category:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
     /**
      * 
      * @param type $slug
@@ -40,7 +112,7 @@ class CategoryController extends Controller
             'page'  =>  $page,
             'jobs_count'    =>  $jobs_count,
             'max_jobs'  => $max_jobs,
-            'route' =>  'cat_show',
+            'route' =>  'category_show',
             'last_page'   => ceil($jobs_count/$max_jobs)
         );
         
@@ -51,5 +123,117 @@ class CategoryController extends Controller
             'pagination'    =>  $pagination
         ));
     }
- 
+
+    /**
+     * Displays a form to edit an existing Category entity.
+     *
+     */
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('JobeetBundle:Category')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Category entity.');
+        }
+
+        $editForm = $this->createEditForm($entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('JobeetBundle:Category:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+    * Creates a form to edit a Category entity.
+    *
+    * @param Category $entity The entity
+    *
+    * @return \Symfony\Component\Form\Form The form
+    */
+    private function createEditForm(Category $entity)
+    {
+        $form = $this->createForm(new CategoryType(), $entity, array(
+            'action' => $this->generateUrl('category_update', array('id' => $entity->getId())),
+            'method' => 'PUT',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Update'));
+
+        return $form;
+    }
+    /**
+     * Edits an existing Category entity.
+     *
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('JobeetBundle:Category')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Category entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createEditForm($entity);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isValid()) {
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('category_edit', array('id' => $id)));
+        }
+
+        return $this->render('JobeetBundle:Category:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+    /**
+     * Deletes a Category entity.
+     *
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $form = $this->createDeleteForm($id);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('JobeetBundle:Category')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find Category entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('category'));
+    }
+
+    /**
+     * Creates a form to delete a Category entity by id.
+     *
+     * @param mixed $id The entity id
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('category_delete', array('id' => $id)))
+            ->setMethod('DELETE')
+            ->add('submit', 'submit', array('label' => 'Delete'))
+            ->getForm()
+        ;
+    }
 }
